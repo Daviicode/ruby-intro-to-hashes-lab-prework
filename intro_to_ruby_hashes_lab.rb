093cd1c055f79390d3bd3b52647f594b12f2a99b
David zhang <d.ydzdavid@yahoo.com>
2019-03-22 21:51:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,10 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads => 
-  
+	monopoly = {
+    :railroads => {:pieces => 4, :names =>{}, :rent_in_dollars => {}
+    }
+	}
 end
 
 def monopoly_with_second_tier
