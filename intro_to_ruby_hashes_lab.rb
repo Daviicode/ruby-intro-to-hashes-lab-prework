7652fe9920813743031b024cf97fe1e4c5180cf5
David zhang <d.ydzdavid@yahoo.com>
2019-03-22 22:13:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,18 +20,27 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {
-	  :rent_in_dollars => {:one_piece_owned : 25, :two_pieces_owned : 50, :three_pieces_owned : 100, :forur_pieces_owned : 200
-	},
-	  :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}
-	  }
+    :railroads => {
+      :pieces => 4, 
+      :names =>{
+        :reading_railroad => {}, 
+        :pennsylvania_railroad => {}, 
+        :b_and_o_railroad => {}, 
+        :shortline => {}
+      }, 
+      :rent_in_dollars => {
+      one_piece_owned: 25, 
+      two_pieces_owned: 50, 
+      three_pieces_owned: 100, 
+      four_pieces_owned: 200
+      }
+      
+    }
+   }
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {
-	  :reading_railroad => {"moretagage_value" => "$100"}, :pennsylvania_railroad => {"moretagage_value" => "$200"},
-	  :b_and_o_railroad => {"moretagage_value" => "$400"},
-	  :shortline => {"moretagage_value" => "$800"}
-	}
+	
 end
 
 
